e681d9b3f61aaef907560544c203086d4e560b7d
Granosik Adrian <adrian.granosik@gmail.com>
2022-02-19 22:43:22 +0100

    load tests again

--- LCT/LCT.k6Tests/Tournament/getTournament_loadTests.js
+++ LCT/LCT.k6Tests/Tournament/getTournament_loadTests.js
@@ -1,38 +1,46 @@
-import { check, sleep } from "k6";
+import { check, group, sleep } from "k6";
 import http from "k6/http";
 import { _basePostParams, _baseTournamentApiUrl } from "../variables.js";
 import { URL } from 'https://jslib.k6.io/url/1.0.0/index.js';
 
 export const options = {
     stages: [
-        { duration: '1m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-        { duration: '2m', target: 200 }, // stay at 100 users for 10 minutes
-        { duration: '1m', target: 0 }, // ramp-down to 0 users
+        { duration: '10s', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+        { duration: '20s', target: 200 }, // stay at 100 users for 10 minutes
+        { duration: '30s', target: 0 }, // ramp-down to 0 users
     ],
     thresholds: {
-        http_req_duration: ['p(99)<700'], // 99% of requests should be below 400ms
+        http_req_duration: ['p(95)<700'],
     },
 }
 
   export default function () {
-    // const id = addTournamentToGet().split('"').join("");
-    const id = "71E78DF7-54D6-470D-5035-08D9F2D06EFC".split('"').join("");
+    var id;
+    group('post is success', function(){
+        const postResult = addTournamentToGet();
+        id = postResult.body.split('"').join("");
+        check(postResult, {
+            'is status 200:': (r) => r.status == 200
+        });
+    })
 
-    const url = new URL(_baseTournamentApiUrl);
-    url.searchParams.append('Id', id);
-    const result = http.get(url.toString());
-    check(result, {
-        'is status 200:': (r) => r.status == 200
-    });
-    // sleep(1);
+    group('get is ok', function(){
+        const url = new URL(_baseTournamentApiUrl);
+        url.searchParams.append('Id', id);
+        const result = http.get(url.toString());
+        check(result, {
+            'is status 200:': (r) => r.status == 200
+        });
+    })
+
 }
-// function addTournamentToGet() {
-//     const rand = Math.floor((Math.random() * 10000) +1);
-//     const payload = JSON.stringify({
-//         name: 'performance test' + rand.toString(),
-//         playerLimit: rand
-//     });
+export function addTournamentToGet() {
+    const rand = Date.now() * Math.random()
+    const payload = JSON.stringify({
+        name: 'performance test' + rand.toString(),
+        playerLimit: 2
+    });
 
-//     const result = http.post(_baseTournamentApiUrl + '/create', payload, _basePostParams);
-//     return result.body;
-// }
+    const result = http.post(_baseTournamentApiUrl + '/create', payload, _basePostParams);
+    return result;
+}
