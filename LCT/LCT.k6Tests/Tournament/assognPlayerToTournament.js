4d62787802e1b46c22cca7c9b12c066effa6d892
Granosik Adrian <adrian.granosik@gmail.com>
2022-02-22 08:00:20 +0100

    assign payer load tests

---
+++ LCT/LCT.k6Tests/Tournament/assognPlayerToTournament.js
@@ -0,0 +1,28 @@
+import { check, group } from "k6"
+import { assignPlayers, postTournament } from "./common.js";
+
+export const options = {
+    stages: [
+        { duration: '10s', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+        { duration: '20s', target: 200 }, // stay at 100 users for 10 minutes
+        { duration: '30s', target: 0 }, // ramp-down to 0 users
+    ],
+    thresholds: {
+        http_req_duration: ['p(95)<1500'],
+    },
+}
+
+export default function () {
+    const postResult = postTournament(20);
+    let id = postResult.body.split('"').join("");
+    check(postResult, {
+        'is status 200:': (r) => r.status == 200
+    });
+    for(var i = 0; i < 20; i++){
+        let result = assignPlayers(id);
+        check(result, {
+            'assign is 200:': (r) => r.status == 200
+        })
+    }
+
+}
--- LCT/LCT.k6Tests/Tournament/common.js
+++ LCT/LCT.k6Tests/Tournament/common.js
@@ -1,13 +1,25 @@
 import http from "k6/http";
 import { _basePostParams, _baseTournamentApiUrl } from "../variables.js";
 
-export function postTournament() {
+export function postTournament(limit = 2) {
     const rand = Date.now() * Math.random()
     const payload = JSON.stringify({
         name: 'performance test' + rand.toString(),
-        playerLimit: 2
+        playerLimit: limit
     });
 
     const result = http.post(_baseTournamentApiUrl + '/create', payload, _basePostParams);
+    return result;
+}
+
+export function assignPlayers(id){
+    const rand = Date.now() * Math.random()
+    const payload = JSON.stringify({
+        name: 'test',
+        surname: 'performance' + rand,
+        tournamentId: id
+    });
+
+    const result = http.post(_baseTournamentApiUrl + '/assignPlayer', payload, _basePostParams);
     return result;
 }
