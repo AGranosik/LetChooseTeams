d7b07152045a59189674f32a8c26da5843857b16
Granosik Adrian <adrian.granosik@gmail.com>
2022-03-23 07:00:26 +0100

    signalr

---
+++ LCT/LCT.Application/Tournaments/Hubs/PlayerAssignedHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace LCT.Application.Tournaments.Hubs
+{
+    public class PlayerAssignedHub : Hub
+    {
+        public async Task SendMessage(Guid tournamentId, Guid playerId)
+        {
+            await Clients.Clients
+        }
+    }
+}
