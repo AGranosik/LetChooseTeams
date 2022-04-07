54d87079499a0965f858fbb4d349f823a6d5b766
Granosik Adrian <adrian.granosik@gmail.com>
2022-04-07 12:33:47 +0200

    select team doimain tests

---
+++ LCT/LCT.IntegrationTests/Teams/DomainTests/SelectTeamDomainTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using LCT.Core.Entites.Tournaments.Entities;
+using LCT.Core.Entites.Tournaments.ValueObjects;
+using LCT.Core.Entities.Tournaments.Types;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace LCT.IntegrationTests.Teams.DomainTests
+{
+    [TestFixture]
+    public class SelectTeamDomainTests
+    {
+        private readonly Player _player = Player.Register(new Name("sdasd"), new Name("hehe"));
+
+        [Test]
+        public void TeamNameCannotBeEmpty()
+        {
+            var func = () => SelectedTeam.Create(_player, string.Empty);
+            func.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void TeamNameNotCorrect()
+        {
+            var func = () => SelectedTeam.Create(_player, "hehe");
+            func.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void TeamNameSuccess()
+        {
+            var func = () => SelectedTeam.Create(_player, TournamentTeamNames._teams.First());
+            func.Should().NotThrow();
+        }
+    }
+}
