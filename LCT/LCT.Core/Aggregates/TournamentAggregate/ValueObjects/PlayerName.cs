43c1b51d234382a549ab30f466f4cf8df14a703f
AGranosik <adrian.granosik@gmail.com>
2022-08-17 09:22:54 +0200

    player name domain tests

--- LCT/LCT.Core/Aggregates/TournamentAggregate/ValueObjects/PlayerName.cs
+++ LCT/LCT.Core/Aggregates/TournamentAggregate/ValueObjects/PlayerName.cs
@@ -1,18 +1,33 @@
-﻿namespace LCT.Core.Aggregates.TournamentAggregate.ValueObjects
+﻿using LCT.Core.Shared;
+using static LCT.Core.Shared.Validation.FieldValidationExtension;
+
+namespace LCT.Core.Aggregates.TournamentAggregate.ValueObjects
 {
-    public class PlayerName : IEquatable<PlayerName>
+    public class PlayerName : ValueType<PlayerName>
     {
         public string Name { get; private set; }
         public string Surname { get; private set; }
         public PlayerName(string name, string surname)
         {
+            Validate(name);
+            Validate(surname);
             Name = name;
             Surname = surname;
         }
-        public bool Equals(PlayerName other)
+
+        private void Validate(string field)
         {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
+            CheckIfNullOrEmpty(field, nameof(PlayerName));
+            CheckFieldLength(field, nameof(PlayerName));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+
+            PlayerName other = obj as PlayerName;
+            if(other == null) return false;
             return Name == other.Name && Surname == other.Surname;
         }
     }
---
+++ LCT/LCT.IntegrationTests/Core/Domain/PlayerNameDomainTests.cs
@@ -0,0 +1,88 @@
+﻿using FluentAssertions;
+using LCT.Core.Aggregates.TournamentAggregate.ValueObjects;
+using LCT.Core.Shared.Exceptions;
+using NUnit.Framework;
+
+namespace LCT.IntegrationTests.Core.Domain
+{
+    [TestFixture]
+    public class PlayerNameDomainTests
+    {
+        [Test]
+        public void PlayerName_CannotBeNull_ThrowsException()
+        {
+            var func = () => new PlayerName(null, null);
+            func.Should().Throw<FieldCannotBeEmptyException>();
+        }
+
+        [Test]
+        public void PlayerName_NameIsNull_ThrowsException()
+        {
+            var func = () => new PlayerName(null, "hehe");
+            func.Should().Throw<FieldCannotBeEmptyException>();
+        }
+
+        [Test]
+        public void PlayerName_SurnameIsNull_ThrowsException()
+        {
+            var func = () => new PlayerName("hehe", null);
+            func.Should().Throw<FieldCannotBeEmptyException>();
+        }
+
+        [Test]
+        public void PlayerName_CannotBeEmpty_ThrowsException()
+        {
+            var func = () => new PlayerName(string.Empty, "hehe");
+            func.Should().Throw<InvalidFieldException>();
+
+            var func2 = () => new PlayerName("hehe", string.Empty);
+            func2.Should().Throw<InvalidFieldException>();
+        }
+
+        [Test]
+        public void PlayerName_CreationSuccess()
+        {
+            var func = () => new PlayerName("hehe", "fiu fiu");
+            func.Should().NotThrow<InvalidFieldException>();
+        }
+
+        [Test]
+        public void PlayerNameCompare_BothAreNull_True()
+        {
+            PlayerName name = null;
+            (name == null).Should().BeTrue();
+            (name != null).Should().BeFalse();
+        }
+
+        [Test]
+        public void PlayerName_OneIsNull_False()
+        {
+            PlayerName name = new PlayerName("hehe", "fiu fiu");
+            PlayerName nullName = null;
+            (name == nullName).Should().BeFalse();
+            (name != nullName).Should().BeTrue();
+            (nullName == name).Should().BeFalse();
+            (nullName != name).Should().BeTrue();
+        }
+
+        [Test]
+        public void PlayerName_DiffValues_NotEqual()
+        {
+            var name = new PlayerName("hehe", "fiu fiu");
+            var name2 = new PlayerName("hehe2", "fiu fiu");
+
+            (name == name2).Should().BeFalse();
+            (name != name2).Should().BeTrue();
+        }
+
+        [Test]
+        public void PlayerName_SameValues_Equal()
+        {
+            var name = new PlayerName("hehe", "fiu fiu");
+            var name2 = new PlayerName("hehe", "fiu fiu");
+
+            (name == name2).Should().BeTrue();
+            (name != name2).Should().BeFalse();
+        }
+    }
+}
