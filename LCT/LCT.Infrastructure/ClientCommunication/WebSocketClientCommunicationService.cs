d456128fae7ab8e0bae4f918c853ce53503ffbe9
AGranosik <adrian.granosik@gmail.com>
2023-08-28 08:54:34 +0200

    publish fallback works begin

--- LCT/LCT.Infrastructure/ClientCommunication/WebSocketClientCommunicationService.cs
+++ LCT/LCT.Infrastructure/ClientCommunication/WebSocketClientCommunicationService.cs
@@ -1,18 +1,14 @@
 ﻿using LCT.Application.Common.Interfaces;
-using LCT.Infrastructure.ClientCommunication.Hubs;
 using LCT.Infrastructure.MessageBrokers.Interfaces;
-using Microsoft.AspNetCore.SignalR;
 
 namespace LCT.Infrastructure.ClientCommunication
 {
     internal class WebSocketClientCommunicationService : IClientCommunicationService
     {
-        private readonly IHubContext<TournamentHub> _hubContext;
         private readonly IMessageBroker _messageBroker;
 
-        public WebSocketClientCommunicationService(IHubContext<TournamentHub> hubContext, IMessageBroker messsageBroker)
+        public WebSocketClientCommunicationService(IMessageBroker messsageBroker)
         {
-            _hubContext = hubContext;
             _messageBroker = messsageBroker;
         }
 
