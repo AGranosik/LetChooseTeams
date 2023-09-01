0f5d57df57a00440c6240bf7c6a5347b69dc2c65
AGranosik <adrian.granosik@gmail.com>
2023-09-01 09:18:26 +0200

    handling connection problem with redis

--- LCT/LCT.Infrastructure/MessageBrokers/RedisMessageBroker.cs
+++ LCT/LCT.Infrastructure/MessageBrokers/RedisMessageBroker.cs
@@ -1,4 +1,5 @@
-﻿using LCT.Infrastructure.ClientCommunication.Hubs;
+﻿using System.Text.RegularExpressions;
+using LCT.Infrastructure.ClientCommunication.Hubs;
 using LCT.Infrastructure.MessageBrokers.Interfaces;
 using LCT.Infrastructure.MessageBrokers.Models;
 using Microsoft.AspNetCore.SignalR;
@@ -16,6 +17,8 @@ namespace LCT.Infrastructure.MessageBrokers
         private readonly RedisSettings _settings;
         private static Dictionary<string, List<string>> _groupConnectionsDicitonary = new();
         private readonly IHubContext<TournamentHub> _hubContext;
+        private List<UnsentMessage> _unsentMessages = new List<UnsentMessage>();
+
         public RedisMessageBroker(RedisSettings redisSettings, IHubContext<TournamentHub> hubContext)
         {
             _settings = redisSettings;
@@ -24,15 +27,7 @@ namespace LCT.Infrastructure.MessageBrokers
         }
         public async Task PublishAsync<T>(string groupId, T message)
         {
-            //create queue
-            // filter to only to message for opened collection
-            // remove only on current state not clear all.
-            var subscriber = GetSubscriber();
-
-            var clients = await subscriber.PublishAsync(groupId, JsonConvert.SerializeObject(message, new JsonSerializerSettings
-            {
-                ContractResolver = new CamelCasePropertyNamesContractResolver()
-            }), CommandFlags.FireAndForget);
+            var clients = await TryPublishAsync(groupId, message);
             Log.Information($@"Message sent via redis. Clients: {clients}. Channel: {groupId}");
         }
 
@@ -66,6 +61,45 @@ namespace LCT.Infrastructure.MessageBrokers
 
             if(connections.Count == 0)
                 await UnsubscribeAsync(connection.GroupId);
+        }
+
+        private async Task<long> TryPublishAsync<T>(string groupId, T message)
+        {
+            if (!_groupConnectionsDicitonary.Any(d => d.Key == groupId))
+                Log.Warning($@"Misssing connection for {groupId}");
+
+            var serializedMessage = JsonConvert.SerializeObject(message, new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+
+            var queuedMessages = _unsentMessages.Where(um => um.GroupId == groupId).ToList();
+            _unsentMessages.RemoveAll(um => queuedMessages.Any(qm => qm.Id == um.Id));
+            queuedMessages.Add(new UnsentMessage(groupId, serializedMessage));
+            long result = 0;
+            try
+            {
+                result = await PublishMessagesAsync(queuedMessages);
+            }
+            catch (Exception ex)
+            {
+                _unsentMessages.AddRange(queuedMessages);
+                Log.Error(ex.ToString());
+            }
+
+            return result;
+        }
+
+        private async Task<long> PublishMessagesAsync(List<UnsentMessage> unsentMessages)
+        {
+            var subscriber = GetSubscriber();
+            long clients = 0;
+            foreach (var queuedMessage in unsentMessages)
+            {
+                var singleCLients = await subscriber.PublishAsync(queuedMessage.GroupId, queuedMessage.SerializedMessage, CommandFlags.FireAndForget);
+                clients += singleCLients;
+            }
+            return clients;
         }
 
         private static bool ConnectionValidation(MessageBrokerConnection connection)
@@ -111,10 +145,23 @@ namespace LCT.Infrastructure.MessageBrokers
 
         private ISubscriber GetSubscriber()
         {
-            if (_connection is null)
+            if (_connection is null || !_connection.IsConnected)
                 TryOpenRedisConnection();
 
             return _connection.GetSubscriber();
         }
     }
+
+    internal class UnsentMessage {
+        public  string GroupId { get; init; }
+        public string SerializedMessage { get; init; }
+        public Guid Id { get; init; }
+        public UnsentMessage (string groupId, string serializedMessage)
+        {
+            GroupId = groupId;
+            SerializedMessage = serializedMessage;
+            Id = Guid.NewGuid();
+        }
+    
+    } 
 }
