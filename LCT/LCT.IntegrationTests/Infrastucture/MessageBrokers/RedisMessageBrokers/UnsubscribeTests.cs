7ecb8f50c78ed0e4245612e5ec4c27948d95c520
AGranosik <adrian.granosik@gmail.com>
2023-09-26 08:26:36 +0200

    unsubscribe tests finished

--- LCT/LCT.IntegrationTests/Infrastucture/MessageBrokers/RedisMessageBrokers/UnsubscribeTests.cs
+++ LCT/LCT.IntegrationTests/Infrastucture/MessageBrokers/RedisMessageBrokers/UnsubscribeTests.cs
@@ -7,6 +7,8 @@ using LCT.Infrastructure.MessageBrokers;
 using Microsoft.AspNetCore.SignalR;
 using Moq;
 using NUnit.Framework;
+using StackExchange.Redis;
+using System;
 
 namespace LCT.IntegrationTests.Infrastucture.MessageBrokers.RedisMessageBrokers
 {
@@ -19,11 +21,15 @@ namespace LCT.IntegrationTests.Infrastucture.MessageBrokers.RedisMessageBrokers
             var redisConnectionMock = new Mock<IRedisConnection>();
             var hubMock = new Mock<IHubContext<TournamentHub>>();
             var rmb = new RedisMessageBroker(redisConnectionMock.Object, hubMock.Object);
+            var subsriber = new Mock<ISubscriber>();
+
+            redisConnectionMock.Setup(m => m.GetSubscriber())
+                .ReturnsAsync(subsriber.Object);
 
             var action = () => rmb.UnsubscribeAsync(new MessageBrokerConnection(null, null));
             await action.Should().NotThrowAsync();
 
-            redisConnectionMock.Invocations.Count.Should().Be(0);
+            subsriber.Invocations.Count.Should().Be(0);
             hubMock.Invocations.Count.Should().Be(0);
         }
 
@@ -33,11 +39,15 @@ namespace LCT.IntegrationTests.Infrastucture.MessageBrokers.RedisMessageBrokers
             var redisConnectionMock = new Mock<IRedisConnection>();
             var hubMock = new Mock<IHubContext<TournamentHub>>();
             var rmb = new RedisMessageBroker(redisConnectionMock.Object, hubMock.Object);
+            var subsriber = new Mock<ISubscriber>();
+
+            redisConnectionMock.Setup(m => m.GetSubscriber())
+                .ReturnsAsync(subsriber.Object);
 
             var action = () => rmb.UnsubscribeAsync(new MessageBrokerConnection(string.Empty, null));
             await action.Should().NotThrowAsync();
 
-            redisConnectionMock.Invocations.Count.Should().Be(0);
+            subsriber.Invocations.Count.Should().Be(0);
             hubMock.Invocations.Count.Should().Be(0);
         }
 
@@ -47,11 +57,15 @@ namespace LCT.IntegrationTests.Infrastucture.MessageBrokers.RedisMessageBrokers
             var redisConnectionMock = new Mock<IRedisConnection>();
             var hubMock = new Mock<IHubContext<TournamentHub>>();
             var rmb = new RedisMessageBroker(redisConnectionMock.Object, hubMock.Object);
+            var subsriber = new Mock<ISubscriber>();
+
+            redisConnectionMock.Setup(m => m.GetSubscriber())
+                .ReturnsAsync(subsriber.Object);
 
             var action = () => rmb.UnsubscribeAsync(new MessageBrokerConnection("groupId", null));
             await action.Should().NotThrowAsync();
 
-            redisConnectionMock.Invocations.Count.Should().Be(0);
+            subsriber.Invocations.Count.Should().Be(0);
             hubMock.Invocations.Count.Should().Be(0);
         }
 
@@ -61,16 +75,101 @@ namespace LCT.IntegrationTests.Infrastucture.MessageBrokers.RedisMessageBrokers
             var redisConnectionMock = new Mock<IRedisConnection>();
             var hubMock = new Mock<IHubContext<TournamentHub>>();
             var rmb = new RedisMessageBroker(redisConnectionMock.Object, hubMock.Object);
+            var subsriber = new Mock<ISubscriber>();
+
+            redisConnectionMock.Setup(m => m.GetSubscriber())
+                .ReturnsAsync(subsriber.Object);
 
             var action = () => rmb.UnsubscribeAsync(new MessageBrokerConnection("groupId", string.Empty));
             await action.Should().NotThrowAsync();
 
-            redisConnectionMock.Invocations.Count.Should().Be(0);
+            subsriber.Invocations.Count.Should().Be(0);
             hubMock.Invocations.Count.Should().Be(0);
         }
 
         [Test]
+        public async Task ConnectionDoesntExist_NotSubscribedBefore_NoException()
+        {
+            var redisConnectionMock = new Mock<IRedisConnection>();
+            var hubMock = new Mock<IHubContext<TournamentHub>>();
+            var rmb = new RedisMessageBroker(redisConnectionMock.Object, hubMock.Object);
+            var subsriber = new Mock<ISubscriber>();
 
+            redisConnectionMock.Setup(m => m.GetSubscriber())
+                .ReturnsAsync(subsriber.Object);
+
+            var action = () => rmb.UnsubscribeAsync(new MessageBrokerConnection("groupId", "userId"));
+            await action.Should().NotThrowAsync();
+
+            subsriber.Invocations.Count.Should().Be(0);
+            hubMock.Invocations.Count.Should().Be(0);
+        }
+
+        [Test]
+        public async Task ConnectionExists_SubscribedBefore_UnsubscribeCalled()
+        {
+            var redisConnectionMock = new Mock<IRedisConnection>();
+            var hubMock = new Mock<IHubContext<TournamentHub>>();
+            var subsriber = new Mock<ISubscriber>();
+
+            redisConnectionMock.Setup(m => m.GetSubscriber())
+                .ReturnsAsync(subsriber.Object);
+
+            var rmb = new RedisMessageBroker(redisConnectionMock.Object, hubMock.Object);
+            var connection = new MessageBrokerConnection("groupId", "userId");
+
+            await rmb.SubscribeAsync(connection);
+
+            var action = () => rmb.UnsubscribeAsync(connection);
+            await action.Should().NotThrowAsync();
+
+            subsriber.Verify(s => s.UnsubscribeAsync(It.Is<RedisChannel>(c => c == connection.GroupId), It.IsAny<Action<RedisChannel, RedisValue>>(), It.Is<CommandFlags>(cf => cf == CommandFlags.None)), Times.Once());
+        }
+
+        [Test]
+        public async Task MultipleConnectionsExist_ConnectionCannotBeClosed_UnsubscribeNotCalled()
+        {
+            var redisConnectionMock = new Mock<IRedisConnection>();
+            var hubMock = new Mock<IHubContext<TournamentHub>>();
+            var subsriber = new Mock<ISubscriber>();
+
+            redisConnectionMock.Setup(m => m.GetSubscriber())
+                .ReturnsAsync(subsriber.Object);
+
+            var rmb = new RedisMessageBroker(redisConnectionMock.Object, hubMock.Object);
+            var connection = new MessageBrokerConnection("groupId", "userId");
+
+            await rmb.SubscribeAsync(connection);
+            await rmb.SubscribeAsync(new MessageBrokerConnection("groupId", "userId2"));
+
+            var action = () => rmb.UnsubscribeAsync(connection);
+            await action.Should().NotThrowAsync();
+
+            subsriber.Verify(s => s.UnsubscribeAsync(It.Is<RedisChannel>(c => c == connection.GroupId), It.IsAny<Action<RedisChannel, RedisValue>>(), It.Is<CommandFlags>(cf => cf == CommandFlags.None)), Times.Never());
+        }
+
+        [Test]
+        public async Task MultipleConnectionsExist_ConnectionSHoulBeClosed_UnsubscribeCalled()
+        {
+            var redisConnectionMock = new Mock<IRedisConnection>();
+            var hubMock = new Mock<IHubContext<TournamentHub>>();
+            var subsriber = new Mock<ISubscriber>();
+
+            redisConnectionMock.Setup(m => m.GetSubscriber())
+                .ReturnsAsync(subsriber.Object);
+
+            var rmb = new RedisMessageBroker(redisConnectionMock.Object, hubMock.Object);
+            var connection = new MessageBrokerConnection("groupId", "userId");
+            var connection2 = new MessageBrokerConnection("groupId", "userId2");
+
+            await rmb.SubscribeAsync(connection);
+            await rmb.SubscribeAsync(connection2);
+
+            await rmb.UnsubscribeAsync(connection);
+            await rmb.UnsubscribeAsync(connection2);
+
+            subsriber.Verify(s => s.UnsubscribeAsync(It.Is<RedisChannel>(c => c == connection.GroupId), It.IsAny<Action<RedisChannel, RedisValue>>(), It.Is<CommandFlags>(cf => cf == CommandFlags.None)), Times.Once());
+        }
 
     }
 }
